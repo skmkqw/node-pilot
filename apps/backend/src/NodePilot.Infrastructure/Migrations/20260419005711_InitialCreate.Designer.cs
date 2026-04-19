0150524ee55a03257c5ef961bc994ea26f3dee64
skmkqw <tkorsakov77@gmail.com>
2026-04-19 02:59:23 +0200

    config: recreate and apply migrations

--- apps/backend/src/NodePilot.Infrastructure/Migrations/20260419005711_InitialCreate.Designer.cs
+++ apps/backend/src/NodePilot.Infrastructure/Migrations/20260419005711_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NodePilot.Infrastructure.Persistence;
 
@@ -10,9 +11,11 @@ using NodePilot.Infrastructure.Persistence;
 namespace NodePilot.Infrastructure.Migrations
 {
     [DbContext(typeof(NodePilotDbContext))]
-    partial class NodePilotDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260419005711_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.6");
