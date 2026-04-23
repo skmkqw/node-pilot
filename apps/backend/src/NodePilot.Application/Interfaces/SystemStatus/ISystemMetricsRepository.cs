11eac5badf2ce5cf98da9400695cb8b086698881
skmkqw <tkorsakov77@gmail.com>
2026-04-23 06:30:50 +0200

    feat: implement GetLatestSuccessfulAsync & GetHistoricalAsync methods in ISystemMetricsRepository

--- apps/backend/src/NodePilot.Application/Interfaces/SystemStatus/ISystemMetricsRepository.cs
+++ apps/backend/src/NodePilot.Application/Interfaces/SystemStatus/ISystemMetricsRepository.cs
@@ -4,5 +4,7 @@ namespace NodePilot.Application.Interfaces.SystemStatus;
 
 public interface ISystemMetricsRepository
 {
+    public Task<SystemMetric?> GetLatestSuccessfulAsync(CancellationToken ct = default);
+    public Task<List<SystemMetric>> GetHistoricalAsync(DateTime start, DateTime end, CancellationToken ct = default);
     public Task SaveAsync(SystemMetric systemMetric, CancellationToken ct = default);
 }
